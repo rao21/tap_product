c8ee86ac559aa95380502e5cfb4091ee2542447c
Rao21 <rao@shiny.team>
2022-02-14 22:42:18 +0500

    Add repository for data.

---
+++ lib/repository/repository.dart
@@ -0,0 +1,20 @@
+import 'package:tap_todo/provider/api.dart';
+
+import '../provider/product_provider.dart';
+
+/* Mulitple repo provider*/
+class Repository {
+  static final Repository _repo = Repository._internal();
+
+  late ProductProvider _productProvider;
+
+  factory Repository() {
+    return _repo;
+  }
+
+  Repository._internal() {
+    _productProvider = ProductProvider();
+  }
+
+  ProductProvider get productProvider => _productProvider;
+}
