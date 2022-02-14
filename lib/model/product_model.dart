ec09091558f2bc6eaa3614ee95161cbb822d4776
Rao21 <rao@shiny.team>
2022-02-14 22:42:35 +0500

    Add product model.

---
+++ lib/model/product_model.dart
@@ -0,0 +1,24 @@
+import 'package:tap_todo/utils/constants.dart';
+
+class Product {
+  String? id;
+  double? price;
+  String? name;
+  String? description;
+
+  Product({this.price, this.name, this.description});
+
+  Product.fromMap(Map snapshot, String? id)
+      : id = id ?? '',
+        price = snapshot['price'] ?? '',
+        name = snapshot['name'] ?? '',
+        description = snapshot['description'] ?? '';
+
+  toJson() {
+    return {
+      "price": price,
+      "name": name,
+      "description": description,
+    };
+  }
+}
